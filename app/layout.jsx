a8a1c0c58fae3a7ed1bfac40068a92eb1267758e
faustomars <faustoms@outlook.com>
2025-05-30 00:06:17 -0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,8 @@
 import "./globals.css";
-
 export const metadata = {
   title: "Fausto Martínez - DJ Crossover",
   description: "DJ ecuatoriano versátil para fiestas y eventos",
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
